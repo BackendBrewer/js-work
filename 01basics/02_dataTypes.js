08ca28987f6fead7489a96d576b1f48051f84192
BackendBrewer <143825083+BackendBrewer@users.noreply.github.com>
2026-05-05 07:44:24 +0000

    Data Types and its return types

---
+++ 01basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all JS code as newer version of javascript
+
+//alert(3 + 3) we are using nodejs, not browser
+
+console.log(3 + 3); console.log("Salman") // it gives output but code readiability should be high
+
+// Data Types
+// number => 2 power 53 
+// string => ""
+// bigint => greater number
+// symbol => unique value assign
+// undefiend
+// null => standlone value
+
+// object
+
+console.log(typeof undefiend); // return type undefiend
+console.log(typeof null); // return type object
