3cd64a3c9d257f599ed41041f85e61f607c7e037
BackendBrewer <143825083+BackendBrewer@users.noreply.github.com>
2026-05-06 05:26:57 +0000

    Conversion of data types

---
+++ 01basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score = "33"
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// it change the value type but give result in NaN which is error
+// "33" => 33
+// "33abc" => NaN
+// boolen => true:1, false:0
+
+let isLoggedIn = 1;
+let BooleanIsLogged = Boolean(isLoggedIn);
+console.log(BooleanIsLogged);
+
+// 1 => true; 0 => false;
+// "" => false
+// "Salman" => true
