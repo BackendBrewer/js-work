c6e3e26a9836184f4dd70936f62984551338398c
BackendBrewer <143825083+BackendBrewer@users.noreply.github.com>
2026-05-06 05:57:38 +0000

    Operations

--- 01basics/03_conversionOperation.js
+++ 01basics/03_conversionOperation.js
@@ -11,8 +11,36 @@ let valueInNumber = Number(score)
 
 let isLoggedIn = 1;
 let BooleanIsLogged = Boolean(isLoggedIn);
-console.log(BooleanIsLogged);
+// console.log(BooleanIsLogged);
 
 // 1 => true; 0 => false;
 // "" => false
 // "Salman" => true
+
+
+// *************Operations***********
+let value = 3
+let negVal = -value
+console.log(negVal);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**3);
+// console.log(2%3);
+
+console.log(+true) // gives output 1
+console.log(true+) // gives error
+console.log(+""); // gives error
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameNumber = 100
+
+++gameNumber;
+
+console.log(gameNumber);
+
